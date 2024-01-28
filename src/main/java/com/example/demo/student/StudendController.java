ea21434f29cd76aa7ed90b657453d7429dd6a18c
Gianluca <gianlucarighini97@gmail.com>
2024-01-28 11:38:43 +0100

    dipendency injection

--- src/main/java/com/example/demo/student/StudendController.java
+++ src/main/java/com/example/demo/student/StudendController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,8 @@ import java.util.List;
 @RequestMapping(path = "api/v1/student")
 public class StudendController {
     private final StudentService studentService;
+
+    @Autowired
     public StudendController(StudentService studentService) {
         this.studentService = studentService;
     }
