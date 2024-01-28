8f30545ea7df81d2e8557d8d1b89edfe951f2ab4
Gianluca <gianlucarighini97@gmail.com>
2024-01-28 23:37:37 +0100

    add JPArepository (data access layer) + fir repository

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +10,14 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     @GetMapping
     public List<Student> getStudents() {
-        return
-                List.of(
-                        new Student(
-                                1L,
-                                "Mariam",
-                                LocalDate.now(),
-                                21,
-                                "Mariam.info@gmail.com"
-                        )
-                );
+        return studentRepository.findAll();
     }
 }
