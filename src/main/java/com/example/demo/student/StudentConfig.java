b79d77cb89f90e36af6d1d92da8e77489789fdd2
Gianluca <gianlucarighini97@gmail.com>
2024-01-29 13:05:46 +0100

    save some students with repo and properties

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.util.Calendar.JANUARY;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+             Student mariam = new Student(
+                    1L,
+                    "Mariam",
+                    LocalDate.of(2000, LocalDate.MAX.getMonth(), 5),
+                    21,
+                    "mariam@gmail.com"
+            );
+            Student alex = new Student(
+                    "Alex",
+                    LocalDate.of(2004, LocalDate.MAX.getMonth(), 5),
+                    21,
+                    "alex@gmail.com"
+            );
+            repository.saveAll(List.of(mariam, alex));
+        };
+    }
+}
